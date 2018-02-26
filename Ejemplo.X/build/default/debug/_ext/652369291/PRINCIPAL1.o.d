dc96a67fc59335c7c449b7914266e7b7c38fb8dd
Andres Saldaña Aguilar <pollyketpo@gmail.com>
2018-02-26 16:47:57 -0600

    :sparkles: example code

---
+++ Ejemplo.X/build/default/debug/_ext/652369291/PRINCIPAL1.o.d
@@ -0,0 +1,6 @@
+build/default/debug/_ext/652369291/PRINCIPAL1.o:  \
+ ../../../../Downloads/PRINCIPAL1.S 
+build/default/debug/_ext/652369291/PRINCIPAL1.o:  \
+./p30F4013.inc  \
+  \
+../../../../Downloads/PRINCIPAL1.S  \
