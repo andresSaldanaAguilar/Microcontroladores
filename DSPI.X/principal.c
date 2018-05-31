88a349319e1605a4bc36997ec1e1c23356c75233
Elioth Monroy <eliothmonroy@yahoo.com.mx>
2018-05-31 14:58:59 -0500

    Nada

--- DSPI.X/principal.c
+++ DSPI.X/principal.c
@@ -4,13 +4,13 @@
  * BROWN OUT RESET, POWER ON RESET Y CODIGO DE PROTECCION
  * BLOQUE 2. EQUIVALENCIAS Y DECLARACIONES GLOBALES
  * BLOQUE 3. ESPACIOS DE MEMORIA: PROGRAMA, DATOS X, DATOS Y, DATOS NEAR
- * BLOQUE 4. CÓDIGO DE APLICACIÓN
+ * BLOQUE 4. Cï¿½DIGO DE APLICACIï¿½N
  * @device: DSPIC30F4013
  * @oscillator: FRC, 7.3728MHz
  */
 #include "p30F4013.h"
 /********************************************************************************/
-/* 						BITS DE CONFIGURACIÓN									*/	
+/* 						BITS DE CONFIGURACIï¿½N									*/	
 /********************************************************************************/
 /* SE DESACTIVA EL CLOCK SWITCHING Y EL FAIL-SAFE CLOCK MONITOR (FSCM) Y SE 	*/
 /* ACTIVA EL OSCILADOR INTERNO (FAST RC) PARA TRABAJAR							*/
@@ -29,11 +29,11 @@
 /* SE ACTIVA EL POWER ON RESET (POR), BROWN OUT RESET (BOR), 					*/	
 /* POWER UP TIMER (PWRT) Y EL MASTER CLEAR (MCLR)								*/
 /* POR: AL MOMENTO DE ALIMENTAR EL DSPIC OCURRE UN RESET CUANDO EL VOLTAJE DE 	*/	
-/* ALIMENTACIÓN ALCANZA UN VOLTAJE DE UMBRAL (VPOR), EL CUAL ES 1.85V			*/
-/* BOR: ESTE MODULO GENERA UN RESET CUANDO EL VOLTAJE DE ALIMENTACIÓN DECAE		*/
+/* ALIMENTACIï¿½N ALCANZA UN VOLTAJE DE UMBRAL (VPOR), EL CUAL ES 1.85V			*/
+/* BOR: ESTE MODULO GENERA UN RESET CUANDO EL VOLTAJE DE ALIMENTACIï¿½N DECAE		*/
 /* POR DEBAJO DE UN CIERTO UMBRAL ESTABLECIDO (2.7V) 							*/
 /* PWRT: MANTIENE AL DSPIC EN RESET POR UN CIERTO TIEMPO ESTABLECIDO, ESTO 		*/
-/* AYUDA A ASEGURAR QUE EL VOLTAJE DE ALIMENTACIÓN SE HA ESTABILIZADO (16ms) 	*/
+/* AYUDA A ASEGURAR QUE EL VOLTAJE DE ALIMENTACIï¿½N SE HA ESTABILIZADO (16ms) 	*/
 /********************************************************************************/
 //_FBORPOR( PBOR_ON & BORV27 & PWRT_16 & MCLR_EN ); 
 // FBORPOR
@@ -42,7 +42,7 @@
 #pragma config BOREN  = PBOR_ON          // PBOR Enable (Enabled)
 #pragma config MCLRE  = MCLR_EN          // Master Clear Enable (Enabled)
 /********************************************************************************/
-/*SE DESACTIVA EL CÓDIGO DE PROTECCIÓN											*/
+/*SE DESACTIVA EL Cï¿½DIGO DE PROTECCIï¿½N											*/
 /********************************************************************************/
 //_FGS(CODE_PROT_OFF);      
 // FGS
@@ -50,7 +50,7 @@
 #pragma config GCP = CODE_PROT_OFF      // General Segment Code Protection (Disabled)
 
 /********************************************************************************/
-/* SECCIÓN DE DECLARACIÓN DE CONSTANTES CON DEFINE								*/
+/* SECCIï¿½N DE DECLARACIï¿½N DE CONSTANTES CON DEFINE								*/
 /********************************************************************************/
 #define EVER 1
 #define MUESTRAS 64
@@ -58,7 +58,7 @@
 /********************************************************************************/
 /* DECLARACIONES GLOBALES														*/
 /********************************************************************************/
-/*DECLARACIÓN DE LA ISR DEL TIMER 1 USANDO __attribute__						*/
+/*DECLARACIï¿½N DE LA ISR DEL TIMER 1 USANDO __attribute__						*/
 /********************************************************************************/
 void __attribute__((__interrupt__)) _T1Interrupt( void );
 
@@ -80,55 +80,27 @@ int y_input[MUESTRAS] __attribute__ ((space(ymemory)));
 /********************************************************************************/
 int var1 __attribute__ ((near));
 
-//Inicialización
+//Inicializaciï¿½n
 void iniPerifericos( void );
-void iniWIFI(void);
-void configWIFI(void);
+void configDSPI(void);
 void RETARDO_1S( void );
 void comandoAT(char msj[]);
 
-char RST[] = {'A','T','+','R','S','T',13,10,0};
-char CWMODE[] = {'A','T','+','C','W','M','O','D','E','=','1',13,10,0};
-char CIPMUX[] = {'A','T','+','C','I','P','M','U','X','=','0',13,10,0};
-char CWJAP[] = {'A','T','+','C','W','J','A','P','=','\"','W','I','F','I','i','p','n','\"',',','\"','\"',13,10,0};
-char CIFSR[] = {'A','T','+','C','I','F','S','R',13,10,0};
-char CIPSTART[] = {'A','T','+','C','I','P','S','T','A','R','T','=','\"','T','C','P','\"',',','\"','1','2','7','.','0','.','0','.','1','\"',',','8','0','0','0',13,10,0};
-char CIPSEND[] = {'A','T','+','C','I','P','S','E','N','D','=','4',13,10,0};
 
 //Variables
 
 int main (void)
 {
     iniPerifericos();
-    
-    //UART1 BAUDIOS:115200
-    U1BRG  = 0; // (1.8432*10^6)/(16*115200) = 0
-    U1MODE = 0X0420; //uart disable, usa los alternos, autobaudaje
-    U1STA  = 0X8000;
-    
-    //UART2 BAUDIOS:115200
-    U2BRG  = 0; // (1.8432*10^6)/(16*115200) = 0
-    U2MODE = 0X0020; //uart disable,no usa los alternos, autobaudaje??
-    U2STA  = 0X8000;
-    
-    //Interrupciones de uart2
-    IFS1bits.U2RXIF= 0;
-    IEC1bits.U2RXIE= 1;
-    
-    iniWIFI();
     configWIFI();
     
-    //Habilitamos el uart1 y uart2
-    U1MODEbits.UARTEN = 1;
-    U2MODEbits.UARTEN = 1;
-
-    U2TXREG = 'H';
-    U2TXREG = 'O';
-    U2TXREG = 'L';
-    U2TXREG = 'A';
+    SPI1CONbits.SPIEN=1;
+    int cont=0;
     
     for(;EVER;)
     { 
+        WR_DAC(cont);
+        cont++;
         Nop();
     }
     
@@ -142,55 +114,22 @@ int main (void)
 /****************************************************************************/
 void iniPerifericos( void )
 {   
-    //Perifericos C
-    PORTC = 0;
+    PORTAbits.TRISA11=1;
     Nop();
-    LATC = 0;
+    PORTDbits.TRISD0=1;
     Nop();
-    //TRISCbits.TRISC13 = 0;
-    //Nop();
-    TRISCbits.TRISC14 = 1;
+    PORTFbits.TRISF3=1;
     Nop();
-    
-    //Perifericos F
-    TRISFbits.TRISF5 = 1; //U2RX
+    PORTFbits.TRISF6=1;
     Nop();
-    TRISFbits.TRISF4 = 0; //U2TX
+    SPI1STAT=0;
     Nop();
-    PORTF = 0;
-    Nop();
-    LATF = 0;
-    Nop();
+    SPI1CON=0X053F;
 }
 
-void iniWIFI(void){
-    PORTBbits.RB8 = 1;
-    
-    RETARDO_1S();
-    RETARDO_1S();
-    RETARDO_1S();
-    
-    PORTDbits.RD1 = 1;
-    
-    RETARDO_1S();
-    
-    PORTDbits.RD1 = 0;
-    
-    RETARDO_1S();
-
-    PORTDbits.RD1 = 1; 
-    
-    RETARDO_1S();  
-}
-
-void configWIFI(void){
-    comandoAT(RST);
-    comandoAT(CWMODE);
-    comandoAT(CIPMUX);
-    comandoAT(CWJAP);
-    comandoAT(CIFSR);
-    comandoAT(CIPSTART);
-    comandoAT(CIPSEND);    
+void configDSPI(void){
+    SPI1STAT=0;
+    SPI1CON=0X053F;
 }
 
 
