309b6494d89e50e19e94851ca4e3127ed34daac4
Elioth Monroy <eliothmonroy@yahoo.com.mx>
2018-06-05 14:43:23 -0500

    Nada

--- DSPI.X/funciones.s
+++ DSPI.X/funciones.s
@@ -10,19 +10,21 @@
 ; * @PARAM:
 ; */
 _RD_WR_SPI:
+    PUSH W0
     MOV	SPI1BUF, W0
     BCLR IFS0, #SPI1IF
 CICLO:
     BTSS IFS0,	#SPI1IF
     GOTO CICLO
     MOV	W0,	SPI1BUF
-    
+    POP W0
     RETURN
 
 /** @brief 
 ; * @PARAM:
 ; */
 _WR_DAC:
+    PUSH W1
     MOV #0X0FFF, W1
     AND  W1, W0, W0
     BSET W0,	#12
@@ -37,5 +39,5 @@ _WR_DAC:
     NOP
     BSET PORTD,	    #LDAC
     NOP
-    
+    POP W1
     RETURN
